a756bbaf003411b832fd0de133f6ca41039a3dd9
sam l <sluongo718@yahoo.com>
2020-09-17 02:36:47 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,16 +7,7 @@ class ChattyStudent < Student
  
  def raise_hand
    super
-   puts "Pick me!"
-   puts "Pick me!"
-   puts "Pick me!"
-   puts "Pick me!"
-   puts "Pick me!"
-   puts "Pick me!"
-   puts "Pick me!"
-   puts "Pick me!"
-   puts "Pick me!"
-   puts "Pick me!"
+  
  end 
   
   
